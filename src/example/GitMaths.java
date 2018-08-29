732f461d11b0d1d55ddf535b359192b0690f6c9e
tst13 <tst13@LYTC-BUS13.ly.fastc.ie>
2018-08-29 14:30:07 +0100

    Added a new subtract method, with two numbers. The total is printed.

--- src/example/GitMaths.java
+++ src/example/GitMaths.java
@@ -2,10 +2,11 @@ package example;
 
 public class GitMaths {
 
-	public GitMaths() {
-
+	public subtract() {
 		
-	
+		int total = 5 - 2;
+		
+		System.out.println("The total is " + total);
 	}
 
 }
